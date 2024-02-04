c897ce6feefee39e4337770714bd1998d45f6041
Patrick Winters <19wintersp@gmail.com>
2024-02-04 18:14:03 +0000

    feat: add logging

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,15 +1,34 @@
-#include <config.h>
+#include <memory>
 
+#include <spdlog/logger.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+#include <config.h>
 #include "plugin.hpp"
 
-Plugin::Plugin(void) : EuroScope::CPlugIn(
-	EuroScope::COMPATIBILITY_CODE,
-	PLUGIN_NAME, PLUGIN_VERSION, PLUGIN_AUTHORS, PLUGIN_LICENCE
-) {
+Plugin::Plugin(void) :
+	EuroScope::CPlugIn(
+		EuroScope::COMPATIBILITY_CODE,
+		PLUGIN_NAME, PLUGIN_VERSION, PLUGIN_AUTHORS, PLUGIN_LICENCE
+	),
+	logger(spdlog::basic_logger_mt(PLUGIN_NAME, PLUGIN_NAME ".log"))
+{
+#ifdef NDEBUG
+	logger->set_level(spdlog::level::trace);
+#else
+	logger->set_level(spdlog::level::info);
+#endif
+
+	logger->info("plugin loaded");
+
 	DisplayUserMessage(
 		PLUGIN_NAME, "Plugin", "Hello, EuroScope!",
 		true, true, true, true, false
 	);
+
+	logger->debug("plugin initialised");
 }
 
-Plugin::~Plugin(void) {}
+Plugin::~Plugin(void) {
+	logger->info("plugin unloaded");
+}
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <memory>
+
 #include <windows.h>
 #include <EuroScopePlugIn.hpp>
+#include <spdlog/logger.h>
 
 namespace EuroScope = EuroScopePlugIn;
 
 class Plugin : public EuroScope::CPlugIn {
+private:
+	std::shared_ptr<spdlog::logger> logger;
+
 public:
 	Plugin(void);
 	~Plugin(void);
